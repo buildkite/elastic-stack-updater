acef4a23002964e39727e1c4f16cd51fcd8299b1
Lachlan Donald <lachlan@buildkite.com>
2017-06-16 16:33:59 +1000

    Add params :/

--- update-stack.sh
+++ update-stack.sh
@@ -38,8 +38,30 @@ stack_follow() {
 ## -------------------------------------------------
 ## main
 
+params=(
+  "ParameterKey=AgentsPerInstance,UsePreviousValue=true"
+  "ParameterKey=BuildkiteAgentRelease,UsePreviousValue=true"
+  "ParameterKey=BuildkiteAgentToken,UsePreviousValue=true"
+  "ParameterKey=BuildkiteApiAccessToken,UsePreviousValue=true"
+  "ParameterKey=BuildkiteOrgSlug,UsePreviousValue=true"
+  "ParameterKey=BuildkiteQueue,UsePreviousValue=true"
+  "ParameterKey=ECRAccessPolicy,UsePreviousValue=true"
+  "ParameterKey=InstanceType,UsePreviousValue=true"
+  "ParameterKey=KeyName,UsePreviousValue=true"
+  "ParameterKey=ManagedPolicyARN,UsePreviousValue=true"
+  "ParameterKey=MaxSize,UsePreviousValue=true"
+  "ParameterKey=MinSize,UsePreviousValue=true"
+  "ParameterKey=RootVolumeSize,UsePreviousValue=true"
+  "ParameterKey=ScaleDownAdjustment,UsePreviousValue=true"
+  "ParameterKey=ScaleDownPeriod,UsePreviousValue=true"
+  "ParameterKey=ScaleUpAdjustment,UsePreviousValue=true"
+  "ParameterKey=SecretsBucket,UsePreviousValue=true"
+  "ParameterKey=SpotPrice,UsePreviousValue=true"
+  )
+
 stack_name="$1"
 
 aws cloudformation update-stack \
   --stack-name "$stack_name" \
-  --template-url "https://s3.amazonaws.com/buildkite-aws-stack/aws-stack.json"
+  --template-url "https://s3.amazonaws.com/buildkite-aws-stack/aws-stack.json" \
+  --parameters "${params[*]}"
