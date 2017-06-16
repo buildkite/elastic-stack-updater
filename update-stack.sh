f57c5a23ff452c7293676a4270568a863d217c71
Lachlan Donald <lachlan@buildkite.com>
2017-06-16 15:31:53 +1000

    Add helper functions and script

---
+++ update-stack.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -euo pipefail
+
+## -------------------------------------------------
+## functions
+
+stack_status() {
+  aws cloudformation describe-stacks --stack-name "$1" --output text --query 'Stacks[].StackStatus'
+}
+
+stack_events() {
+  aws cloudformation describe-stack-events --stack-name "$1" --output table --query 'sort_by(StackEvents, &Timestamp)[].[
+    EventId,
+    ResourceStatus
+  ]' | sed 1,2d
+}
+
+stack_failures() {
+  aws cloudformation describe-stack-events --stack-name "$1" --output table --query \
+    "sort_by(StackEvents, &Timestamp)[?ResourceStatus=='CREATE_FAILED'].[LogicalResourceId,ResourceStatusReason]" \
+  | sed 1,2d
+}
+
+stack_follow() {
+  until status=$(stack_status "$1"); [[ $status =~ (FAILED|COMPLETE) ]] ; do
+    echo "Stack status is $status, continuing to poll"
+    sleep 20
+  done
+  if [[ $status =~ FAILED ]] ; then
+    stack_events "$1"
+    echo -e "\033[33;31mStack creation failed!\n$(stack_failures "$1")\033[0m"
+    return 1
+  else
+    echo -e "\033[33;32mStack completed successfully\033[0m"
+  fi
+}
+
+## -------------------------------------------------
+## main
+
+aws cloudformation list-stacks
