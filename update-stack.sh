b2cc3503c2a86a99a521b600bcb678d7fbd77bd8
Lachlan Donald <lachlan@buildkite.com>
2017-06-17 15:41:32 +1000

    Wait for previous updates to finish

--- update-stack.sh
+++ update-stack.sh
@@ -26,6 +26,10 @@ stack_failures() {
 
 stack_name="$1"
 
+echo "--- :cloudformation: Waiting for any previous updates to complete"
+aws cloudformation wait stack-update-complete \
+  --stack-name "$stack_name"
+
 echo "--- :lambda: Invoking updateElasticStack function"
 output=$(aws lambda invoke \
   --invocation-type RequestResponse \
